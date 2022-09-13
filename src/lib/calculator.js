b404b006d714c91c084b831c879b1b659e30ceee
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-09-13 15:11:18 -0300

    refact: does change type error

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -4,7 +4,7 @@ function sum(num1, num2) {
   const { isNaN } = Number;
 
   if (isNaN(a) || isNaN(b)) {
-    throw new Error("Nan was provided");
+    throw new TypeError("Nan was provided");
   }
 
   return a + b;
